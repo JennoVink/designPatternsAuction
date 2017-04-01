cd95a413c9db3d475a01dcbb22b36dae62a3d0e9
JennoVink <jenno-vink@hotmail.com>
2017-04-01 12:28:50 +0200

    wat?

--- src/noPattern/Daniel.java
+++ src/noPattern/Daniel.java
@@ -17,7 +17,7 @@ public class Daniel {
 		 * - concrete bidders (BrokeBidder is a good example to see what needs to be done)
 		 * - some decorators
 		 * - proxy pattern
-		 * 
+		 *  
 		 * Always prefer println's and default values over Exception throwing.
 		 * The main method is in the Auctioneer class.
 		 * Note that Product needs an extra param Icon.
